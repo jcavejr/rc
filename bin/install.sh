5b8686f65c786a3c9f109b75128b4cb1a02bcf57
Jeff Cave <jcave@getguru.com>
2024-09-19 10:34:25 -0400

    Setup shell_aliases in install script

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,16 @@
 set -e pipefail
 
 function main() {
-    echo "TODO"
+    script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    cwd=$(pwd)
+    cd $script_dir
+    rc_repo_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+    cd ${cwd}
+    echo "Installing rc repository from ${rc_repo_dir}"
+
+    user_rc_dir=${HOME}/.zsh
+    mkdir -pv ${user_rc_dir}
+    ln -sfv ${rc_repo_dir}/shell_aliases ${user_rc_dir}/shell_aliases
 }
 
 main "$@"
